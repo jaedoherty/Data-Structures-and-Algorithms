6546a1d83add20ff88fbaba715048424771e8707
Jae Doherty <jaeld97@gmail.com>
2021-02-06 21:02:53 -0800

    solved is anagram?

--- feb06.js
+++ feb06.js
@@ -84,3 +84,53 @@ function uncommonFromSentences(A, B) {
 // let A = "apple apple"
 // let B = "banana"
 // console.log(uncommonFromSentences(A, B))
+
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+
+*/
+
+function isAnagram(s, t) {
+    if (s.length !== t.length) return false;
+
+    let count = {};
+
+    for(let i = 0; i < s.length; i++) {
+        if (count[s[i]]) {
+            count[s[i]] += 1;
+        } else {
+            count[s[i]] = 1;
+        }
+    }
+
+    for(let i = 0; i < t.length; i++) {
+        if (count[t[i]]) {
+            count[t[i]] -= 1;
+        } else {
+            count[t[i]] = 1;
+        }
+    }
+
+    return Object.values(count).every(num => num === 0)
+
+}
+
+let s = "anagram"; 
+let t = "nagaram" 
+// let v = "nagaram";
+console.log(isAnagram(s, t))
